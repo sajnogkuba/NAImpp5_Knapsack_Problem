4d25959fe9cebe0a6bffdead56ea0d08108183a7
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-05-22 22:12:21 +0200

    read backpack capacity and number of items

--- NAImpp5_Knapsack_Problem/src/Main.java
+++ NAImpp5_Knapsack_Problem/src/Main.java
@@ -1,8 +1,10 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please input path to file with data: ");
         String dataPath = scanner.nextLine();
@@ -13,5 +15,10 @@ public class Main {
             dataPath = scanner.nextLine();
             dataFile = new File(dataPath);
         }
+        Scanner dataFileScanner = new Scanner(dataFile);
+        String backpackData = dataFileScanner.nextLine();
+        String[] backpackDataArr = backpackData.split(" ");
+        int backpackCapacity = Integer.parseInt(backpackDataArr[0]);
+        int numberOfItems = Integer.parseInt(backpackDataArr[1]);
     }
 }
